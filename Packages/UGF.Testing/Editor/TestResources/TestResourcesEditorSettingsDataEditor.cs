424acf28dd17fca82ac04d4dc9268fe8bf74e3d4
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-08-06 20:38:22 +0300

    Update TestResourcesEditorSettingsDataEditor.cs

--- Packages/UGF.Testing/Editor/TestResources/TestResourcesEditorSettingsDataEditor.cs
+++ Packages/UGF.Testing/Editor/TestResources/TestResourcesEditorSettingsDataEditor.cs
@@ -1,4 +1,6 @@
-﻿using UGF.EditorTools.Editor.IMGUI.Scopes;
+﻿using System.IO;
+using UGF.AssetBundles.Editor;
+using UGF.EditorTools.Editor.IMGUI.Scopes;
 using UnityEditor;
 using UnityEngine;
 
@@ -7,6 +9,7 @@ namespace UGF.Testing.Editor.Editor.TestResources
     [CustomEditor(typeof(TestResourcesEditorSettingsData), true)]
     internal class TestResourcesEditorSettingsDataEditor : UnityEditor.Editor
     {
+        private readonly AssetBundleFileDrawer m_assetBundleDrawer = new AssetBundleFileDrawer();
         private SerializedProperty m_propertyBuildBeforeTestRun;
         private SerializedProperty m_propertyClearAfterTestRun;
         private SerializedProperty m_propertyAssetBundleName;
@@ -14,9 +17,13 @@ namespace UGF.Testing.Editor.Editor.TestResources
         private SerializedProperty m_propertyAssetBundleOutputDirectory;
         private SerializedProperty m_propertyAssetBundleOutputPath;
         private TestResourcesEditorSettingsDataFolderListDrawer m_listFolders;
+        private bool m_displayAssetBundleInfo;
 
         private void OnEnable()
         {
+            m_assetBundleDrawer.DisplayMenuClear = false;
+            m_assetBundleDrawer.Enable();
+
             m_propertyBuildBeforeTestRun = serializedObject.FindProperty("m_buildBeforeTestRun");
             m_propertyClearAfterTestRun = serializedObject.FindProperty("m_clearAfterTestRun");
             m_propertyAssetBundleName = serializedObject.FindProperty("m_assetBundleName");
@@ -26,10 +33,13 @@ namespace UGF.Testing.Editor.Editor.TestResources
 
             m_listFolders = new TestResourcesEditorSettingsDataFolderListDrawer(serializedObject.FindProperty("m_folders"));
             m_listFolders.Enable();
+
+            UpdateAssetBundleDrawer();
         }
 
         private void OnDisable()
         {
+            m_assetBundleDrawer.Disable();
             m_listFolders.Disable();
         }
 
@@ -49,16 +59,24 @@ namespace UGF.Testing.Editor.Editor.TestResources
 
             EditorGUILayout.Space();
 
+            bool exists = TestResourcesEditorUtility.IsAssetBundleExists();
+
             using (new EditorGUILayout.HorizontalScope())
             {
                 GUILayout.FlexibleSpace();
 
-                using (new EditorGUI.DisabledScope(!TestResourcesEditorUtility.IsAssetBundleExists()))
+                m_displayAssetBundleInfo = GUILayout.Toggle(m_displayAssetBundleInfo, "Display AssetBundle Information");
+
+                EditorGUILayout.Space();
+
+                using (new EditorGUI.DisabledScope(!exists))
                 {
                     if (GUILayout.Button("Clear", GUILayout.Width(75F)))
                     {
                         TestResourcesEditorUtility.ClearAssetBundle();
                         AssetDatabase.Refresh();
+
+                        UpdateAssetBundleDrawer();
                     }
                 }
 
@@ -68,10 +86,38 @@ namespace UGF.Testing.Editor.Editor.TestResources
                     {
                         TestResourcesEditorUtility.BuildAssetBundle();
                         AssetDatabase.Refresh();
+
+                        UpdateAssetBundleDrawer();
                     }
                 }
 
                 EditorGUILayout.Space();
+            }
+
+            EditorGUILayout.Space();
+
+            if (m_displayAssetBundleInfo)
+            {
+                if (m_assetBundleDrawer.HasData)
+                {
+                    m_assetBundleDrawer.DrawGUILayout();
+                }
+                else
+                {
+                    EditorGUILayout.HelpBox("No AssetBundle information available, build required.", MessageType.Info);
+                }
+            }
+        }
+
+        private void UpdateAssetBundleDrawer()
+        {
+            m_assetBundleDrawer.Clear();
+
+            string path = TestResourcesEditorSettings.GetAssetBundlePath();
+
+            if (File.Exists(path))
+            {
+                m_assetBundleDrawer.Set(path);
             }
         }
     }
