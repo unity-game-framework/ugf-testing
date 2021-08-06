7143308937db4b9731ee3494ec28416fc9207515
AlexanderVorobyov <vorobyovvorobyov@gmail.com>
2021-08-06 19:34:31 +0300

    Add TestResourcesProvider

---
+++ Packages/UGF.Testing/Runtime/TestResources/TestResourcesSettings.cs
@@ -0,0 +1,22 @@
+﻿using UGF.CustomSettings.Runtime;
+using UGF.RuntimeTools.Runtime.Storage;
+
+namespace UGF.Testing.Runtime.TestResources
+{
+    public static class TestResourcesSettings
+    {
+        public static CustomSettingsPackage<TestResourcesSettingsAsset> Settings { get; } = new CustomSettingsPackage<TestResourcesSettingsAsset>
+        (
+            "UGF.Testing",
+            nameof(TestResourcesSettings)
+        );
+
+        public static string GetAssetBundlePath()
+        {
+            TestResourcesSettingsAsset data = Settings.GetData();
+            string path = StorageUtility.GetPath(data.AssetBundleDirectory, data.AssetBundlePath);
+
+            return path;
+        }
+    }
+}
